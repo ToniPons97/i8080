59b23c12bf0e8989d648a7af93ff7ef2c3809f6f
Antonio Pons <toninopons@gmail.com>
2024-06-16 14:00:48 +0200

    Code cleaning.

--- src/main.c
+++ src/main.c
@@ -7,9 +7,7 @@ size_t get_file_size(FILE *file);
 int disassemble8080Opcode(unsigned char *buffer, int pc);
 
 int main(int argc, char **argv) {
-
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("[!] Binary not provided...\n");
         printf("Example usage: ./%s pong.bin\n", argv[0]);
 
@@ -23,29 +21,24 @@ int main(int argc, char **argv) {
     printf("Dissasembling %s\n", argv[1]);
 
     file = fopen(argv[1], "rb");
-
     if (file == NULL) {
         perror("Unable to read file.");
         return 1;
     }
 
     file_buff_size = get_file_size(file);
-
     if (file_buff_size == -1) {
         perror("Error getting buffer size. Exiting program.");
         return 1;
     }
 
     buffer = (unsigned char*) calloc(file_buff_size, sizeof(unsigned char));
-
     if (buffer == NULL) {
         perror("could not allocate memory dynamically.");
         return 1;
     }
 
     fread(buffer, file_buff_size, 1, file);
-
-    //print_file(buffer, file_buff_size);
 
     for (int i = 0; i < file_buff_size; i++) {
         disassemble8080Opcode(buffer, i);
@@ -65,16 +58,13 @@ void print_file(unsigned char* buffer, size_t size) {
 size_t get_file_size(FILE *file) {
     size_t size;
 
-    if (file == NULL)
-    {
+    if (file == NULL) {
         perror("File pointer is null.");
         return -1;
     }
 
     fseek(file, 0, SEEK_END);
-
     size = ftell(file);
-
     rewind(file);
 
     return size;
