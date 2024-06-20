2790ea4a4a2388f70e11e6a7edd6f5dbd42efe97
Antonio Pons <apons@arkeero.com>
2024-06-20 13:22:19 +0200

    Fixing formatting.

--- src/i8080_cpu.c
+++ src/i8080_cpu.c
@@ -1132,6 +1132,6 @@ void print_cpu_status(State8080* state) {
     printf("\nA: 0x%.2x\nB: 0x%.2x\nC: 0x%.2x\nD: 0x%.2x\nE: 0x%.2x\nH: 0x%.2x\nL: 0x%.2x\nSP: 0x%.4x\n", 
         state->a, state->b, state->c, state->d, state->e, state->h, state->l, state->sp);
     
-    printf("\nZ: 0x%.2x\nS: 0x%.2x\nCY: 0x%.2x\nAC: 0x%.2x\nP: 0x%.2x\n\n", 
-        state->cc.z, state->cc.s, state->cc.cy, state->cc.ac, state->cc.p);
+    printf("\nZ: 0x%.2x\nS: 0x%.2x\nCY: 0x%.2x\nAC: 0x%.2x\nP: 0x%.2x\nPC: 0x%.4x\n\n", 
+        state->cc.z, state->cc.s, state->cc.cy, state->cc.ac, state->cc.p, state->pc);
 }
