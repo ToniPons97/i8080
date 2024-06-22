ffeb7dcc17f78e9d846a47f9340fcce5886f8185
Antonio Pons <toninopons@gmail.com>
2024-06-22 11:01:06 +0200

    Refactoring.

--- src/i8080_cpu.c
+++ src/i8080_cpu.c
@@ -33,15 +33,9 @@ void emulate_i8080(State8080* state) {
             state->b = (bc >> 8) & 0xff;
             state->c = bc & 0xff;
             break; 
-        }  
-        case 0x04: {                  // INR B
-            inr(state, &state->b);
-            break;
-        }  
-        case 0x05: {                  // DCR B
-            dcr(state, &state->b);
-            break;
-        }  
+        }
+        case 0x04: inr(state, &state->b); break;            // INR B  
+        case 0x05: dcr(state, &state->b); break;            // DCR B
         case 0x06: {                  // MVI B, D8
             state->b = opcode[1];
             state->pc += 1;
@@ -70,14 +64,8 @@ void emulate_i8080(State8080* state) {
             state->c = bc & 0xff;
             break;    
         }
-        case 0x0c: {                  // INR C
-            inr(state, &state->c);
-            break;
-        }    
-        case 0x0d: {                  // DCR C
-            dcr(state, &state->c);
-            break;
-        }   
+        case 0x0c: inr(state, &state->c); break;            // INR C
+        case 0x0d: dcr(state, &state->c); break;            // DCR C  
         case 0x0e: {                  // MVI C,D8 
             state->c = opcode[1];
             state->pc += 1;
@@ -108,14 +96,8 @@ void emulate_i8080(State8080* state) {
             state->e = de & 0xff;
             break;
         }    
-        case 0x14: {                  // INR D
-            inr(state, &state->d);
-            break;
-        }    
-        case 0x15: {                  // DCR D
-            dcr(state, &state->d);
-            break;
-        }    
+        case 0x14: inr(state, &state->d); break;            // INR D   
+        case 0x15: dcr(state, &state->d); break;            // DCR D    
         case 0x16: {                  // MVI D, D8
             state->d = opcode[1];
             state->pc += 1;
@@ -145,14 +127,8 @@ void emulate_i8080(State8080* state) {
             state->e = de & 0xff;
             break;
         }    
-        case 0x1c: {                  // INR E
-            inr(state, &state->e);
-            break;
-        }    
-        case 0x1d: {                  // DCR E
-            dcr(state, &state->e);
-            break; 
-        }   
+        case 0x1c: inr(state, &state->e); break;            // INR E 
+        case 0x1d: dcr(state, &state->e); break;            // DCR E   
         case 0x1e: {                  // MVI E,D8 
             state->e = opcode[1];
             state->pc += 1;
@@ -184,14 +160,8 @@ void emulate_i8080(State8080* state) {
             state->l = hl & 0xff;
             break;
         }    
-        case 0x24: {                  // INR H
-            inr(state, &state->h);
-            break;
-        }    
-        case 0x25: {                  // DCR H
-            dcr(state, &state->h);
-            break;
-        }    
+        case 0x24: inr(state, &state->h); break;            // INR H   
+        case 0x25: dcr(state, &state->h); break;            // DCR H    
         case 0x26: {                  // MVI H,D8 
             state->h = opcode[1];
             state->pc += 1;
@@ -218,23 +188,14 @@ void emulate_i8080(State8080* state) {
             state->l = hl & 0xff;
             break;
         }   
-        case 0x2c: {                  // INR L
-            inr(state, &state->l);
-            break; 
-        }   
-        case 0x2d: {                  // DCR L
-            dcr(state, &state->l);
-            break;
-        }    
+        case 0x2c: inr(state, &state->l); break;            // INR L  
+        case 0x2d: dcr(state, &state->l); break;            // DCR L    
         case 0x2e: {                  // MVI L, D8 
             state->l = opcode[1];
             state->pc += 1;
             break;
         }    
-        case 0x2f: {                  // CMA
-            state->a = ~state->a;
-            break;
-        }  
+        case 0x2f: state->a = ~state->a; break;              // CMA 
         case 0x30: break;    
         case 0x31: {                  // LXI SP, D16
             uint16_t address = (opcode[2] << 8) | opcode[1];
@@ -248,10 +209,7 @@ void emulate_i8080(State8080* state) {
             state->pc += 2;
             break;
         }    
-        case 0x33: {                  // INX SP
-            state->sp += 1;
-            break;
-        }    
+        case 0x33: state->sp += 1; break;                   // INX SP    
         case 0x34: {                  // INR M
             uint16_t address = (state->h << 8) | state->l;
             inr(state, &state->memory[address]);
@@ -268,15 +226,9 @@ void emulate_i8080(State8080* state) {
             state->pc += 1;
             break;
         }
-        case 0x37: {                  // STC
-            state->cc.cy = 1;
-            break;
-        }    
+        case 0x37: state->cc.cy = 1; break;                 // STC    
         case 0x38: break;    
-        case 0x39: {                  // DAD SP
-            dad(state, state->sp);
-            break;
-        }   
+        case 0x39: dad(state, state->sp); break;            // DAD SP 
         case 0x3a: {                  // LDA adr
             uint16_t address = (opcode[2] << 8) | opcode[1];
             state->a = state->memory[address];
@@ -300,208 +252,79 @@ void emulate_i8080(State8080* state) {
             state->pc += 1;
             break;
         }    
-        case 0x3f: {                  // CMC
-            state->cc.cy = ~state->cc.cy;
-            break;
-        }    
-        case 0x40: {                  // MOV B,B
-            state->b = state->b;
-            break;
-        }    
-        case 0x41: {                  // MOV B,C
-            state->b = state->c;
-            break;
-        }    
-        case 0x42: {                 // MOV B,D
-            state->b = state->d;
-            break; 
-        }   
-        case 0x43: {                  // MOV B,E
-            state->b = state->e;
-            break;
-        }    
-        case 0x44: {                  // MOV B,H
-            state->b = state->h;
-            break;
-        }    
-        case 0x45: {                  // MOV B,L
-            state->b = state->l;
-            break;
-        }    
+        case 0x3f: state->cc.cy = ~state->cc.cy; break;     // CMC   
+        case 0x40: state->b = state->b; break;              // MOV B,B  
+        case 0x41: state->b = state->c; break;              // MOV B,C    
+        case 0x42: state->b = state->d; break;              // MOV B,D   
+        case 0x43: state->b = state->e; break;              // MOV B,E 
+        case 0x44: state->b = state->h; break;              // MOV B,H   
+        case 0x45: state->b = state->l; break;              // MOV B,L    
         case 0x46: {                  // MOV B,M
             uint16_t address = (state->h << 8) | state->l;
             state->b = state->memory[address];
             break;
         }    
-        case 0x47: {                  // MOV B,A
-            state->b = state->a;
-            break;
-        }    
-        case 0x48: {                  // MOV C,B
-            state->c = state->b;
-            break;
-        }    
-        case 0x49: {                  // MOV C,C
-            state->c = state->c;
-            break; 
-        }   
-        case 0x4a: {                 // MOV C,D
-            state->c = state->d;
-            break;  
-        }  
-        case 0x4b: {                  // MOV C,E
-            state->c = state->e;
-            break; 
-        }   
-        case 0x4c: {                 // MOV C,H
-            state->c = state->h;
-            break;  
-        }  
-        case 0x4d: {                  // MOV C,L
-            state->c = state->l;
-            break;
-        }    
+        case 0x47: state->b = state->a; break;              // MOV B,A   
+        case 0x48: state->c = state->b; break;              // MOV C,B   
+        case 0x49: state->c = state->c; break;              // MOV C,C  
+        case 0x4a: state->c = state->d; break;              // MOV C,D
+        case 0x4b: state->c = state->e; break;              // MOV C,E   
+        case 0x4c: state->c = state->h; break;              // MOV C,H  
+        case 0x4d: state->c = state->l; break;              // MOV C,L    
         case 0x4e: {                  // MOV C,M
             uint16_t address = (state->h << 8) | state->l;
             state->c = state->memory[address];
             break; 
         }   
-        case 0x4f: {                  // MOV C,A
-            state->c = state->a;
-            break; 
-        }   
-        case 0x50: {                  // MOV D,B
-            state->d = state->b;
-            break;
-        }    
-        case 0x51: {                  // MOV D,C
-            state->d = state->c;
-            break;
-        }    
-        case 0x52: {                  // MOV D,D 
-            state->d = state->d;
-            break; 
-        }   
-        case 0x53: {                  // MOV D,E
-            state->d = state->e;
-            break;
-        }    
-        case 0x54: {                  // MOV D,H
-            state->d = state->h;
-            break;
-        }    
-        case 0x55: {                  // MOV D,L
-            state->d = state->l;
-            break;
-        }    
+        case 0x4f: state->c = state->a; break;              // MOV C,A  
+        case 0x50: state->d = state->b; break;              // MOV D,B   
+        case 0x51: state->d = state->c; break;              // MOV D,C    
+        case 0x52: state->d = state->d; break;              // MOV D,D  
+        case 0x53: state->d = state->e; break;              // MOV D,E  
+        case 0x54: state->d = state->h; break;              // MOV D,H  
+        case 0x55: state->d = state->l; break;              // MOV D,L   
         case 0x56: {                  // MOV D,M
             uint16_t address = (state->h << 8) | state->l;
             state->d = state->memory[address];
             break;
         }    
-        case 0x57: {                  // MOV D,A
-            state->d = state->a;
-            break;
-        }    
-        case 0x58: {                  // MOV E,B
-            state->e = state->b;
-            break;
-        }    
-        case 0x59: {                  // MOV E,C
-            state->e = state->c;
-            break;
-        }    
-        case 0x5a: {                  // MOV E,D
-            state->e = state->d;
-            break;
-        }    
-        case 0x5b: {                  // MOV E,E
-            state->e = state->e;
-            break;
-        }    
-        case 0x5c: {                  // MOV E,H
-            state->e = state->h;
-            break; 
-        }   
-        case 0x5d: {                  // MOV E,L
-            state->e = state->l;
-            break;
-        }    
+        case 0x57: state->d = state->a; break;              // MOV D,A
+        case 0x58: state->e = state->b; break;              // MOV E,B
+        case 0x59: state->e = state->c; break;              // MOV E,C
+        case 0x5a: state->e = state->d; break;              // MOV E,D  
+        case 0x5b: state->e = state->e; break;              // MOV E,E 
+        case 0x5c: state->e = state->h; break;              // MOV E,H
+        case 0x5d: state->e = state->l; break;              // MOV E,L
         case 0x5e: {                  // MOV E,M
             uint16_t address = (state->h << 8) | state->l;
             state->e = state->memory[address];
             break;
         }    
-        case 0x5f: {                  // MOV E,A
-            state->e = state->a;
-            break;
-        }   
-        case 0x60: {                  // MOV H,B
-            state->h = state->b;
-            break; 
-        }   
-        case 0x61: {                 // MOV H,C
-            state->h = state->c;
-            break; 
-        }   
-        case 0x62: {                 // MOV H,D
-            state->h = state->b;
-            break;
-        }    
-        case 0x63: {                 // MOV H,E
-            state->h = state->e;
-            break;
-        }    
-        case 0x64: {                 // MOV H,H
-            state->h = state->h;
-            break; 
-        }   
-        case 0x65: {                 // MOV H,L
-            state->h = state->l;
-            break; 
-        }   
+        case 0x5f: state->e = state->a; break;              // MOV E,A
+        case 0x60: state->h = state->b; break;              // MOV H,B
+        case 0x61: state->h = state->c; break;              // MOV H,C
+        case 0x62: state->h = state->b; break;              // MOV H,D
+        case 0x63: state->h = state->e; break;              // MOV H,E
+        case 0x64: state->h = state->h; break;              // MOV H,H
+        case 0x65: state->h = state->l; break;              // MOV H,L
         case 0x66: {                 // MOV H,M
             uint16_t address = (state->h << 8) | state->l;
             state->h = state->memory[address];
             break;  
         }  
-        case 0x67: {                // MOV H,A
-            state->h = state->a;
-            break;  
-        }  
-        case 0x68: {                 // MOV L,B
-            state->l = state->b;
-            break;
-        }    
-        case 0x69: {                 // MOV L,C
-            state->l = state->c;
-            break;
-        }    
-        case 0x6a: {                 // MOV L,D
-            state->l = state->d;
-            break;
-        }    
-        case 0x6b: {                 // MOV L,E
-            state->l = state->e;
-            break;
-        }    
-        case 0x6c: {                 // MOV L,H
-            state->l = state->h;
-            break;
-        }    
-        case 0x6d: {                 // MOV L,L
-            state->l = state->l;
-            break;
-        }    
+        case 0x67: state->h = state->a; break;              // MOV H,A
+        case 0x68: state->l = state->b; break;              // MOV L,B
+        case 0x69: state->l = state->c; break;              // MOV L,C
+        case 0x6a: state->l = state->d; break;              // MOV L,D
+        case 0x6b: state->l = state->e; break;              // MOV L,E
+        case 0x6c: state->l = state->h; break;              // MOV L,H
+        case 0x6d: state->l = state->l; break;              // MOV L,L
         case 0x6e: {                 // MOV L,M
             uint16_t address = (state->h << 8) | state->l;
             state->l = state->memory[address];
             break;
         }    
-        case 0x6f: {                 // MOV L,A
-            state->l = state->a;
-            break; 
-        }   
+        case 0x6f: state->l = state->a; break;              // MOV L,A
         case 0x70: {                  // MOV M,B
             uint16_t address = (state->h << 8) | state->l;
             state->memory[address] = state->b;
