78af143587ce886ca08bc16a0f7410c4f2ba534f
Antonio Pons <toninopons@gmail.com>
2025-12-27 20:22:38 +0100

    loading test value in memory for cpudiag

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "cpu.h"
 #include "debugger.h"
 #include "display.h"
 #include "si_machine_io.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 
 void print_banner(void);
@@ -21,20 +21,20 @@ size_t rom_buffer_size = 0;
 
 int main(int argc, char** argv) {
     print_banner();
-    
-    switch(handle_program_init(argc, argv)) {
-        case 0:
-            run_space_invaders();
-            break;
-        
-        case 1:
-            debug_space_invaders();
-            break;
 
-        default:
-            printf("Running CPUDIAG\n\n");
-            run_cpudiag();
-            return 1;
+    switch (handle_program_init(argc, argv)) {
+    case 0:
+        run_space_invaders();
+        break;
+
+    case 1:
+        debug_space_invaders();
+        break;
+
+    default:
+        printf("Running CPUDIAG\n\n");
+        run_cpudiag();
+        return 1;
     }
 
     return 0;
@@ -60,7 +60,9 @@ void debug_space_invaders() {
 
         read(STDIN_FILENO, &key, 1);
 
-        if (handle_debugger_commands(state, key, &instruction_count, &rom_buffer_size, &original, MAIN_WINDOW, MAIN_RENDERER)) {
+        if (handle_debugger_commands(state, key, &instruction_count,
+                                     &rom_buffer_size, &original, MAIN_WINDOW,
+                                     MAIN_RENDERER)) {
             counter = instruction_count;
         } else {
             while (counter++ < instruction_count) {
@@ -90,10 +92,8 @@ void run_space_invaders() {
     State8080* state = init_8080_state();
     load_space_invaders_rom(state, &rom_buffer_size);
 
-    IOInterface io = {
-        .machine_in = si_machine_in,
-        .machine_out = si_machine_out
-    };
+    IOInterface io = {.machine_in = si_machine_in,
+                      .machine_out = si_machine_out};
 
     KeyboardMap key_state;
     init_keyboard_state(&key_state);
@@ -110,7 +110,7 @@ void run_space_invaders() {
 
     while (!quit) {
         handle_sdl_events(&key_state, &event, &quit);
-        
+
         // Emulate CPU cycles until the next expected interrupt
         long long now = get_current_time();
         while (get_current_time() < next_interrupt_time) {
@@ -130,7 +130,8 @@ void run_space_invaders() {
 
         // Update timing for next interrupt
         last_interrupt_time = now;
-        next_interrupt_time = last_interrupt_time + 8000; // 8ms for 2 interrupts in 16ms frame
+        next_interrupt_time =
+            last_interrupt_time + 8000; // 8ms for 2 interrupts in 16ms frame
 
         // Render the screen at VBlank
         if (which_interrupt == 1) {
@@ -146,8 +147,6 @@ void run_space_invaders() {
     sdl_cleanup(MAIN_WINDOW, MAIN_RENDERER);
 }
 
-
-
 void run_cpudiag(void) {
     set_raw_mode(&original);
 
@@ -160,14 +159,18 @@ void run_cpudiag(void) {
 
     read_file_at_offset(state, "./cpudiag", 0x100, &buffer_size);
     printf("Buffer size: %ld\n", buffer_size);
-    
-    state->pc = 0x100;
-    state->memory[368] = 0x7; 
 
-    while ((state->pc < buffer_size) && !should_exit) {
+    state->pc = 0x100;
+    state->memory[368] = 0x7;
+    state->memory[0xde02] = 0xAA;
+
+    //(state->pc < buffer_size) &&
+    while (!should_exit) {
         read(STDIN_FILENO, &key, 1);
 
-        if (handle_debugger_commands(state, key, &instruction_count, &buffer_size, &original, MAIN_WINDOW, MAIN_RENDERER)) {
+        if (handle_debugger_commands(state, key, &instruction_count,
+                                     &buffer_size, &original, MAIN_WINDOW,
+                                     MAIN_RENDERER)) {
             counter = instruction_count;
         } else {
             while (counter++ < instruction_count) {
@@ -193,51 +196,63 @@ int handle_program_init(int argc, char** argv) {
         printf("Example usage: %s space-invaders.bin\n", argv[0]);
         return -1;
     }
-    
+
     char invaders[] = "invaders";
     char debug_flag[] = "--debug";
 
     switch (argc) {
-        case 2:
-            if (strncmp(invaders, argv[1], sizeof(invaders) / sizeof(invaders[0])) == 0) {
-                printf("Running Space Invaders...\n");
-                return 0;
-            } else {
-                printf("Should run some other file if it exists.\n");
-                return -1;
-            }
-        
-        case 3:
-            if (strncmp(invaders, argv[1], sizeof(invaders) / sizeof(invaders[0])) == 0 && 
-                strncmp(debug_flag, argv[2], sizeof(debug_flag) / sizeof(debug_flag[0])) == 0) {
-                    printf("Should run space invaders in debug mode.\n");
-                    return 1;
-            } else if (strncmp(debug_flag, argv[2], sizeof(debug_flag) / sizeof(debug_flag[0])) == 0) {
-                printf("Should run some other file in debug mode if it exists.\n");
-                return -1;
-            } else {
-                printf("Should tell you to fuck off.\n");
-                return -1;
-            }
-
-        default:
+    case 2:
+        if (strncmp(invaders, argv[1],
+                    sizeof(invaders) / sizeof(invaders[0])) == 0) {
+            printf("Running Space Invaders...\n");
+            return 0;
+        } else {
+            printf("Should run some other file if it exists.\n");
             return -1;
+        }
+
+    case 3:
+        if (strncmp(invaders, argv[1],
+                    sizeof(invaders) / sizeof(invaders[0])) == 0 &&
+            strncmp(debug_flag, argv[2],
+                    sizeof(debug_flag) / sizeof(debug_flag[0])) == 0) {
+            printf("Should run space invaders in debug mode.\n");
+            return 1;
+        } else if (strncmp(debug_flag, argv[2],
+                           sizeof(debug_flag) / sizeof(debug_flag[0])) == 0) {
+            printf("Should run some other file in debug mode if it exists.\n");
+            return -1;
+        } else {
+            printf("Should tell you to fuck off.\n");
+            return -1;
+        }
+
+    default:
+        return -1;
     }
 }
 
 long long get_current_time(void) {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return ((long long) tv.tv_sec * 1000000LL) + (tv.tv_usec);
+    return ((long long)tv.tv_sec * 1000000LL) + (tv.tv_usec);
 }
 
 void print_banner(void) {
-    printf("==============================================================================\n");
-    printf("=                                                                            =\n");
-    printf("=                          Intel 8080 Emulator                               =\n");
-    printf("=                       Disassembler and Debugger                            =\n");
-    printf("=                                                                            =\n");
-    printf("=                       Developed by Toni Pons                               =\n");
-    printf("=                                                                            =\n");
-    printf("==============================================================================\n\n");
+    printf("==================================================================="
+           "===========\n");
+    printf("=                                                                  "
+           "          =\n");
+    printf("=                          Intel 8080 Emulator                     "
+           "          =\n");
+    printf("=                       Disassembler and Debugger                  "
+           "          =\n");
+    printf("=                                                                  "
+           "          =\n");
+    printf("=                       Developed by Toni Pons                     "
+           "          =\n");
+    printf("=                                                                  "
+           "          =\n");
+    printf("==================================================================="
+           "===========\n\n");
 }
